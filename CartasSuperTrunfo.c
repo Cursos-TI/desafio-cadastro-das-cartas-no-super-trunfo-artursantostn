9faa89f43f242dd07d18fe9c7e089583ec3f414d
Artur Santos <artur.santos.tn1@gmail.com>
2025-08-12 01:43:26 +0000

    Commit Changes

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,127 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    // ESTADO 1
+    char estado_um[20];
+    char codigo_carta_estado_um[20];
+    char nome_cidade_estado_um[20];
+
+    int populacao_estado_um, pontos_turisticos_estado_um;
+
+    float area_estado_um;
+    float PIB_estado_um;
+
+    // ESTADO 2
+    char estado_dois[20];
+    char codigo_carta_estado_dois[20];
+    char nome_cidade_estado_dois[20];
+
+    int populacao_estado_dois, pontos_turisticos_estado_dois;
+
+    float area_estado_dois;
+    float PIB_estado_dois;
+    
+    printf("\n");
+    printf("----------------------------------------------------\n");
+    printf("\n");
+
+    printf("VAMOS CADASTRAR OS DADOS DA PRIMEIRA CIDADE\n");
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", nome_cidade_estado_um);
+
+    printf("-----------\n");
+
+    printf("Digite o Estado desta Cidade: \n");
+    scanf("%s", estado_um);
+
+    printf("-----------\n");
+
+    printf("Digite o Codigo desta Cidade: \n");
+    scanf("%s", codigo_carta_estado_um);
+
+    printf("-----------\n");
+
+    printf("Digite a População desta Cidade: \n");
+    scanf(" %d", &populacao_estado_um);
+
+    printf("-----------\n");
+
+    printf("Digite o Número de Pontos Turísticos tem esta Cidade: \n");
+    scanf("%d", &pontos_turisticos_estado_um);
+    
+    printf("-----------\n");
+
+    printf("Digite a Área em metros quadrados desta Cidade: \n");
+    scanf("%f", &area_estado_um);
+    
+    printf("-----------\n");
+
+    printf("Digite o Produto Interno Bruto (PIB) desta Cidade: \n");
+    scanf("%f", &PIB_estado_um);
+
+    printf("\n");
+    printf("----------------------------------------------------\n");
+    printf("\n");
+
+    printf("VAMOS CADASTRAR OS DADOS DA SEGUNDA CIDADE\n");
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", nome_cidade_estado_dois);
+
+    printf("-----------\n");
+
+    printf("Digite o Estado desta Cidade: \n");
+    scanf("%s", estado_dois);
+
+    printf("-----------\n");
+
+    printf("Digite o Codigo desta Cidade: \n");
+    scanf("%s", codigo_carta_estado_dois);
+
+    printf("-----------\n");
+
+    printf("Digite a População desta Cidade: \n");
+    scanf(" %d", &populacao_estado_dois);
+
+    printf("-----------\n");
+
+    printf("Digite o Número de Pontos Turísticos tem esta Cidade: \n");
+    scanf("%d", &pontos_turisticos_estado_dois);
+    
+    printf("-----------\n");
+
+    printf("Digite a Área em metros quadrados desta Cidade: \n");
+    scanf("%f", &area_estado_dois);
+    
+    printf("-----------\n");
+
+    printf("Digite o Produto Interno Bruto (PIB) desta Cidade: \n");
+    scanf("%f", &PIB_estado_dois);
+    
+    printf("\n");
+    printf("----------------------------------------------------\n");
+
+    printf("Carta 1\n");
+    printf("Codigo da Carta: %s\n", codigo_carta_estado_um);
+    printf("---\n");
+    printf("Cidade: %s.\n", nome_cidade_estado_um);
+    printf("Estado: %s.\n", estado_um);
+    printf("População: %d Habitantes.\n", populacao_estado_um);
+    printf("Área: %f km².\n", area_estado_um);
+    printf("Produto Interno Bruto (PIB): %.2f Milhões de Reais.\n", PIB_estado_um);
+    printf("Número de Pontos Turísticos: %d\n.", pontos_turisticos_estado_um);
+
+    printf("\n");
+    printf("----------------------------------------------------\n");
+
+    printf("Carta 2\n");
+    printf("Codigo da Carta: %s\n", codigo_carta_estado_dois);
+    printf("---\n");
+    printf("Cidade: %s.\n", nome_cidade_estado_dois);
+    printf("Estado: %s.\n", estado_dois);
+    printf("População: %d Habitantes.\n", populacao_estado_dois);
+    printf("Área: %f km².\n", area_estado_dois);
+    printf("Produto Interno Bruto (PIB): %.2f Milhões de Reais.\n", PIB_estado_dois);
+    printf("Número de Pontos Turísticos: %d\n.", pontos_turisticos_estado_dois);
+
     return 0;
 }
