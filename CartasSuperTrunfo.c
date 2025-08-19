4ff20f145dcfd84202bbca0220c44dc766c57399
Artur Santos <artur.santos.tn1@gmail.com>
2025-08-18 22:42:19 -0300

    Update CartasSuperTrunfo.c Nivel Mestre
    Codigo Melhorado para o nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <locale.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
@@ -18,19 +19,25 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
     // ESTADO 1
     char estado_um[20];
     char codigo_carta_estado_um[20];
     char nome_cidade_estado_um[20];
-    int populacao_estado_um, pontos_turisticos_estado_um;
+    unsigned long int populacao_estado_um;
+    int pontos_turisticos_estado_um;
     float area_estado_um, PIB_estado_um, densidade_populacional_estado_um, PIB_percapita_estado_um;
+    float super_poder_estado_um;
 
     // ESTADO 2
     char estado_dois[20];
     char codigo_carta_estado_dois[20];
     char nome_cidade_estado_dois[20];
-    int populacao_estado_dois, pontos_turisticos_estado_dois;
+    unsigned long int populacao_estado_dois;
+    int pontos_turisticos_estado_dois;
     float area_estado_dois, PIB_estado_dois, densidade_populacional_estado_dois, PIB_percapita_estado_dois;
+    float super_poder_estado_dois;
 
     printf("\n----------------------------------------------------\n\n");
 
@@ -49,7 +56,7 @@ int main() {
     printf("-----------\n");
 
     printf("Digite a População desta Cidade: \n");
-    scanf("%d", &populacao_estado_um);
+    scanf("%lu", &populacao_estado_um);
     printf("-----------\n");
 
     printf("Digite o Número de Pontos Turísticos desta Cidade: \n");
@@ -80,7 +87,7 @@ int main() {
     printf("-----------\n");
 
     printf("Digite a População desta Cidade: \n");
-    scanf("%d", &populacao_estado_dois);
+    scanf("%lu", &populacao_estado_dois);
     printf("-----------\n");
 
     printf("Digite o Número de Pontos Turísticos desta Cidade: \n");
@@ -96,12 +103,17 @@ int main() {
 
     printf("\n----------------------------------------------------\n");
 
-    // Cálculos
+    // Cálculos Densidade Populacional
     densidade_populacional_estado_um = populacao_estado_um / area_estado_um;
     densidade_populacional_estado_dois = populacao_estado_dois / area_estado_dois;
-
+    
+    // Cálculos PIB Per Capita
     PIB_percapita_estado_um = (PIB_estado_um * 1000000000) / populacao_estado_um; // conversão p/ reais
     PIB_percapita_estado_dois = (PIB_estado_dois * 1000000000) / populacao_estado_dois;
+
+    // Cálculos do Super Poder
+    super_poder_estado_um = (float)populacao_estado_um + area_estado_um + PIB_estado_um + pontos_turisticos_estado_um + PIB_percapita_estado_um + (1.0f / densidade_populacional_estado_um);
+    super_poder_estado_dois = (float)populacao_estado_dois + area_estado_dois + PIB_estado_dois + pontos_turisticos_estado_dois + PIB_percapita_estado_dois + (1.0f / densidade_populacional_estado_dois);
 
     // Saída Carta 1
     printf("Carta 1\n");
@@ -109,12 +121,13 @@ int main() {
     printf("---\n");
     printf("Cidade: %s.\n", nome_cidade_estado_um);
     printf("Estado: %s.\n", estado_um);
-    printf("População: %d Habitantes.\n", populacao_estado_um);
+    printf("População: %lu Habitantes.\n", populacao_estado_um);
     printf("Área: %.2f km².\n", area_estado_um);
     printf("Produto Interno Bruto (PIB): %.2f bilhões de reais.\n", PIB_estado_um);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos_estado_um);
     printf("Densidade Populacional: %.2f hab/km².\n", densidade_populacional_estado_um);
     printf("PIB Per Capita: %.2f reais.\n", PIB_percapita_estado_um);
+    printf("Super Poder %s: %.2f pontos.\n", nome_cidade_estado_um, super_poder_estado_um);
 
     printf("\n----------------------------------------------------\n");
 
@@ -124,12 +137,25 @@ int main() {
     printf("---\n");
     printf("Cidade: %s.\n", nome_cidade_estado_dois);
     printf("Estado: %s.\n", estado_dois);
-    printf("População: %d Habitantes.\n", populacao_estado_dois);
+    printf("População: %lu Habitantes.\n", populacao_estado_dois);
     printf("Área: %.2f km².\n", area_estado_dois);
     printf("Produto Interno Bruto (PIB): %.2f bilhões de reais.\n", PIB_estado_dois);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos_estado_dois);
     printf("Densidade Populacional: %.2f hab/km².\n", densidade_populacional_estado_dois);
     printf("PIB Per Capita: %.2f reais.\n", PIB_percapita_estado_dois);
+    printf("Super Poder %s: %.2f pontos.\n", nome_cidade_estado_dois, super_poder_estado_dois);
+    printf("\n----------------------------------------------------\n");
+    
+    // Comparações
+    printf("Comparação de Cartas:\n\n");
+
+    printf("População: Carta %d venceu\n", (populacao_estado_um > populacao_estado_dois) ? 1 : 2);
+    printf("Área: Carta %d venceu\n", (area_estado_um > area_estado_dois) ? 1 : 2);
+    printf("PIB: Carta %d venceu\n", (PIB_estado_um > PIB_estado_dois) ? 1 : 2);
+    printf("Pontos Turísticos: Carta %d venceu\n", (pontos_turisticos_estado_um > pontos_turisticos_estado_dois) ? 1 : 2);
+    printf("Densidade Populacional: Carta %d venceu\n", (densidade_populacional_estado_um < densidade_populacional_estado_dois) ? 1 : 2);
+    printf("PIB per Capita: Carta %d venceu\n", (PIB_percapita_estado_um > PIB_percapita_estado_dois) ? 1 : 2);
+    printf("Super Poder: Carta %d venceu\n", (super_poder_estado_um > super_poder_estado_dois) ? 1 : 2);
 
     return 0;
 }
